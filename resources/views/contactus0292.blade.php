efa78cc894816374bb08bcbcbb23cd870bd216eb
Lukman M <lukmanmuhamadilham@gmail.com>
2021-05-22 11:47:11 +0700

    tambah file contact us

---
+++ resources/views/contactus0292.blade.php
@@ -0,0 +1,93 @@
+@extends ('layout/template')
+
+@section('title','contact us')
+
+@section('konten')
+    <div class="container">
+        <img src="foto.png" class="rounded-circle mx-auto d-block" alt="..." style="width: 20%;height: auto;">
+        <br>
+        <br>
+        <div class="container">
+            <div class="row">
+              <div class="col">
+                
+              </div>
+              <div class="col">
+                <div class="container">
+                  <div class="row">
+                    <div class="col">
+                      <img src="phone.png" class="rounded d-block " alt="..." style="display: block;max-width: 35px;height: auto;">
+                    </div>
+                    <div class="col float-start">
+                      <h5>082245851632</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                
+              </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <div class="container">
+            <div class="row">
+              <div class="col">
+                
+              </div>
+              <div class="col">
+                <div class="container">
+                  <div class="row">
+                    <div class="col">
+                      <img src="email.png" class="rounded d-block " alt="..." style="display: block;max-width: 35px;height: auto;">
+                    </div>
+                    <div class="col">
+                      <h5>okakakikio@gmail.com</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                
+              </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <div class="container text-center">
+          <div class="row">
+            <div class="col">
+              Follow me :
+            </div>
+          </div>
+        </div>
+        <br>
+        <div class="container text-center">
+            <div class="row">
+              <div class="col">
+                
+              </div>
+              <div class="col">
+                <div class="container text-center">
+                  <div class="row">
+                    <div class="col">
+                      <a href="#">
+                        <img src="facebook.png" class="rounded d-inline-block m-2" alt="..." style="max-width: 35px;height: auto;">
+                      </a>
+                    </div>
+                    <div class="col">
+                      <a href="#">
+                        <img src="instagram.png" class="rounded d-inline-block m-2" alt="..." style="max-width: 35px;height: auto;">
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                
+              </div>
+            </div>
+        </div>
+    </div>
+@endsection
